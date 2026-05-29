6b4fc133446e08a29513f32d630e941534188bde
Daniel Victor <skidan007@gmail.com>
2026-05-29 16:01:53 +0100

    week 3 js code added and checked

--- week-three-project/script.js
+++ week-three-project/script.js
@@ -1,2 +1,62 @@
-const name = prompt("Enter your name:");
-alert("Hello, " + name + "!");
+// =========================
+// Responsive Navigation Toggle
+// =========================
+
+const navigationLinks = document.querySelectorAll(".nav-list a");
+const navigationToggle = document.querySelector(".navigation-toggle");
+
+navigationLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        navigationToggle.checked = false;
+    });
+});
+
+// =========================
+// Dynamic Hero Button Interaction
+// =========================
+
+
+const headingText = document.querySelector(".hero-text h2");
+const heroButton = document.querySelector(".primary-button");
+
+heroButton.addEventListener("click", () => {
+    headingText.textContent = "Thanks for Exploring Our Services!";
+});
+
+// =========================
+// Service Card Interaction
+// =========================
+
+const serviceCards = document.querySelectorAll(".service-card");
+
+serviceCards.forEach((card) => {
+
+    card.addEventListener("mouseenter", () => {
+        const serviceTitle = card.querySelector("h3");
+        serviceTitle.style.color = "#007BFF";
+        card.style.transform = "translateY(-8px)";
+        card.style.transition = "0.3s ease";
+        card.style.fontWeight = "bold";
+        // card.style.color = "#007BFF";
+    });
+
+    card.addEventListener("mouseleave", () => {
+        const serviceTitle = card.querySelector("h3");
+        card.style.transform = "translateY(0)";
+        card.style.fontWeight = "normal";
+        serviceTitle.style.color = "#111827";
+    });
+
+
+});
+
+// =========================
+// Dynamic Footer Year
+// =========================
+
+const footerText = document.querySelector(".footer-content p");
+
+const currentYear = new Date().getFullYear();
+
+footerText.textContent =
+    `© ${currentYear} Modern Web Solutions. All rights reserved.`;
